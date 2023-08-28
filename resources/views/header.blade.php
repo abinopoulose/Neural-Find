2b3b7aeb933e52a474559928e2a99ab070d33730
Abino Poulose <abinopoulose@outlook.com>
2023-08-28 12:03:49 +0530

    Title

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,8 +5,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100&display=swap" rel="stylesheet">
     <link href="{{asset('css/main.css')}}" rel="stylesheet">
-   
-
+    <link rel="icon" type="image/x-icon" href="{{asset('images/img.jpg')}}"> 
+    <title>NeuralDoc</title>   
 </head>
 <body>
 
